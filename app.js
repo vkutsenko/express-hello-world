ad05ca2d5ffae3198f0c1737770ba9e48eb98f6e
Куценко Вячеслав Васильевич <ext.vkutsenko@tinkoff.ru>
2024-01-17 13:11:43 +0300

    Add request

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const axios = require('axios');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send('work'));
+app.get("/", (req, res) =>
+{
+    
+    return res.type('html').send('work');
+});
+
+app.get("/get_data", (req, res, next) => {
+    console.log("'/get_data' call");
+    axios.get("https://line52w.bk6bba-resources.com/line/desktop/topEvents3?place=live&sysId=1&lang=ru&scopeMarket=1600")
+        .then(data => { 
+            console.log(data.data)
+            res.json(data.data)
+        })
+        .catch(err => next(err));
+  })
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
